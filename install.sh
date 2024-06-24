5f61e5695d3cea9bbf73806a27658bfe5b2fc36e
Juliano Liberato <juliano@9level.com.br>
2024-06-24 14:26:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,14 @@
 sudo apt update
 sudo apt install -y vim git curl
 
+echo 'HISTTIMEFORMAT="%d/%m/%Y - %T => "' >> ~/.bashrc
+echo "HISTSIZE=50000" >> ~/.bashrc
+echo "HISTFILESIZE=100000" >> ~/.bashrc
+echo "shopt -s histappend" >> ~/.bashrc
+echo "export HISTCONTROL=ignoredups:erasedups" >> ~/.bashrc
+cp ~/.bashrc{,.orig}
+echo "PS1='\[\033[00;32m\]┌─[\[\033[00;31m\]\u\[\033[00m\]@\[\033[01;34m\]\h\[\033[00;32m\]]──[\[\033[01;33m\]\A\[\033[00;32m\]]──[\[\033[00m\]\w\[\033[00;32m\]]\n└─[\!]─>\$\[\033[00;32m\] '" >> ~/.bashrc
+
 # Clone the lightline.vim plugin into the appropriate directory
 git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
 
